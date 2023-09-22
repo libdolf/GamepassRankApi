a9631bdc51afd261b6b167003f109ccbe45ea595
lindolfo <lindolfomoizinho1@gmail.com>
2023-09-22 16:48:36 -0300

    Commit inicial

---
+++ src/main/java/com/libdolf/gamepassrankapi/GamepassRankApiApplication.java
@@ -0,0 +1,13 @@
+package com.libdolf.gamepassrankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamepassRankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamepassRankApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/libdolf/gamepassrankapi/GamepassGameRankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libdolf.gamepassrankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamepassGameRankApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
