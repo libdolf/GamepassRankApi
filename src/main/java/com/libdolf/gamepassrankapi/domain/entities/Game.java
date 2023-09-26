6ed3e9d5b60cb8587ac1409324c0d124363d6899
Lindolfo Moizinho <lindolfomoizinho1@gmail.com>
2023-09-26 19:00:27 -0300

    Atributo categories concertado

--- src/main/java/com/libdolf/gamepassrankapi/domain/entities/Game.java
+++ src/main/java/com/libdolf/gamepassrankapi/domain/entities/Game.java
@@ -1,11 +1,15 @@
 package com.libdolf.gamepassrankapi.domain.entities;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 @Data
 @AllArgsConstructor
@@ -17,7 +21,7 @@ public class Game {
     private String developerName;
     private String publisherName;
     private LocalDateTime releaseDate;
-    private String categories;
+    private List<String> categories = new ArrayList<>();
     private Double reviewScore;
     private Integer reviewCount;
 }
