125685667b1b6545aa9cc193bc2650b801a81e90
Lindolfo Moizinho <lindolfomoizinho1@gmail.com>
2023-09-26 19:01:32 -0300

    Novos endpoints criados

--- src/main/java/com/libdolf/gamepassrankapi/data/controllers/GetAllController.java
+++ src/main/java/com/libdolf/gamepassrankapi/data/controllers/GetAllController.java
@@ -5,19 +5,33 @@ import com.libdolf.gamepassrankapi.domain.usecases.GetGamesListUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/")
+@RequestMapping("/v1")
 @AllArgsConstructor
 public class GetAllController {
     private GetGamesListUseCase useCase;
-    @GetMapping
-    public ResponseEntity<List<Game>> getAll(){
+    @GetMapping("/best")
+    public ResponseEntity<List<Game>> getBest(){
         List<Game> games = useCase.getBestGames();
         return ResponseEntity.ok(games);
     }
+
+    @GetMapping("/worst")
+    public ResponseEntity<List<Game>> getWorst(){
+        List<Game> games = useCase.getWorstGames();
+        return ResponseEntity.ok(games);
+    }
+
+    @GetMapping("/{gameName}")
+    public ResponseEntity<List<Game>> getGameByName(@PathVariable String gameName){
+        List<Game> game = useCase.getGamesByName(gameName);
+        return ResponseEntity.ok(game);
+    }
+
 }
