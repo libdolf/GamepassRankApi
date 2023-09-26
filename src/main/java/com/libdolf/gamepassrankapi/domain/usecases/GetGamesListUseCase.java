d9a9c319d0714bfbde4bc432db23984e89decaa9
Lindolfo Moizinho <lindolfomoizinho1@gmail.com>
2023-09-26 19:02:09 -0300

    Novos métodos  para as novas endpoints implementados

--- src/main/java/com/libdolf/gamepassrankapi/domain/usecases/GetGamesListUseCase.java
+++ src/main/java/com/libdolf/gamepassrankapi/domain/usecases/GetGamesListUseCase.java
@@ -6,6 +6,7 @@ import com.libdolf.gamepassrankapi.domain.entities.Game;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -16,7 +17,23 @@ public class GetGamesListUseCase {
     public List<Game> getBestGames() {
        List<Game> games = gateway.getAll();
         Collections.sort(games, Comparator.comparingDouble(Game::getReviewScore).reversed());
-
         return games;
+    }
+
+    public List<Game> getWorstGames() {
+        List<Game> games = gateway.getAll();
+        Collections.sort(games, Comparator.comparingDouble(Game::getReviewScore));
+        return games;
+    }
+
+    public List<Game> getGamesByName(String gameName) {
+        List<Game> games = gateway.getAll();
+        List<Game> gamesNew = new ArrayList<>();
+        for (Game game : games){
+            if(game.getTitle().toLowerCase().contains(gameName.toLowerCase())){
+                gamesNew.add(game);
+            }
+        }
+        return gamesNew;
     }
 }
